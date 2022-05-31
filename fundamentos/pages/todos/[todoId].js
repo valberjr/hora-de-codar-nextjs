ee470a1a2b3c6b504cb5d66732e8cd5319f52f85
valberjr <3cifhzht@duck.com>
2022-05-31 09:52:47 -0300

    rotas dinâmicas com dados

--- fundamentos/pages/todos/[todoId].js
+++ fundamentos/pages/todos/[todoId].js
@@ -1,30 +1,61 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
-export default function Todo() {
-    const router = useRouter();
+export async function getStaticProps(context) {
+    const { params } = context;
 
-    const todoId = router.query.todoId;
+    const data = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${params.todoId}`
+    );
 
+    const todo = await data.json();
+
+    return {
+        props: {
+            todo,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+
+    const data = await response.json();
+
+    const paths = data.map((todo) => {
+        return {
+            params: {
+                todoId: `${todo.id}`,
+            },
+        };
+    });
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export default function Todo({ todo }) {
     return (
         <>
-            <Link href="/">Voltar</Link>
-            <h1>Exibindo o todo: {todoId}</h1>
+            <Link href="/todos">Voltar</Link>
+            <h1>Exibindo o todo: {todo.id}</h1>
+            <h3>Texto: {todo.title}</h3>
             <p>
                 Comentário: la la la...
-                <Link href={`/todos/${todoId}/comments/1`}>
+                <Link href={`/todos/${todo.id}/comments/1`}>
                     <a>Detalhes</a>
                 </Link>
             </p>
             <p>
                 Comentário: le le le...
-                <Link href={`/todos/${todoId}/comments/2`}>
+                <Link href={`/todos/${todo.id}/comments/2`}>
                     <a>Detalhes</a>
                 </Link>
             </p>
             <p>
                 Comentário: li li li...
-                <Link href={`/todos/${todoId}/comments/3`}>
+                <Link href={`/todos/${todo.id}/comments/3`}>
                     <a>Detalhes</a>
                 </Link>
             </p>
--- fundamentos/pages/todos/index.js
+++ fundamentos/pages/todos/index.js
@@ -1,5 +1,5 @@
 import styles from '../../styles/Todos.module.css';
-
+import Link from 'next/link';
 export async function getStaticProps() {
     const data = await fetch('https://jsonplaceholder.typicode.com/todos');
 
@@ -16,7 +16,10 @@ export default function Todos({ todos }) {
             <h1>Tarefas para fazer</h1>
             <ul className={styles.todolist}>
                 {todos.map((todo) => (
-                    <li key={todo.id}>{todo.title}</li>
+                    <li key={todo.id}>
+                        {todo.title} -{' '}
+                        <Link href={`/todos/${todo.id}`}>Ver mais</Link>{' '}
+                    </li>
                 ))}
             </ul>
         </>
