184bb44d136313cc9a1ebfa843b67ec4b9693c77
valberjr <3cifhzht@duck.com>
2022-05-31 08:50:19 -0300

    assets

--- fundamentos/pages/index.js
+++ fundamentos/pages/index.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css';
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -15,6 +16,12 @@ export default function Home() {
             <div className={styles.title}>
                 <h1>Hello World Next.js</h1>
             </div>
+            <Image
+                src="/images/city.jpg"
+                width="300px"
+                height="400px"
+                alt="Cidade a noite"
+            />
         </>
     );
 }
